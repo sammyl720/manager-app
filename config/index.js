991e70abc75b2e4200743e88dd20baaaf9fcd0d0
sam leider <sam.leider@hotmail.com>
2020-07-09 15:42:09 -0400

    quick fix

--- config/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyDu0vFo3_OkQN_v1hEPFUm-rHxYtH9M8OQ',
-  authDomain: 'manager-3d5f8.firebaseapp.com',
-  databaseURL: 'https://manager-3d5f8.firebaseio.com',
-  projectId: 'manager-3d5f8',
-  storageBucket: 'manager-3d5f8.appspot.com',
-  messagingSenderId: '210449567176',
-  appId: '1:210449567176:web:5d2355e318832a6656c4fd'
-}
